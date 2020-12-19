5bb6ac97457b66edcf58ff2e69a2ca63213fcee8
Bryan Romero <bryanromerou@gmail.com>
2020-12-18 18:12:49 -0800

    changed the default tempo for arrayToBeat

--- src/functions/arrayToBeat.js
+++ src/functions/arrayToBeat.js
@@ -1,13 +1,22 @@
 import {Howl} from 'howler';
 import Timer from 'tiny-timer';
 
-export default (arr=[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],type="Kick", tempo=300) =>{
+export default (arr=[0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1],type="Kick", tempo=500) =>{
+    // The smaller the number the faster the tempo
     const timer = new Timer({ interval: tempo, stopwatch: false });
     let sound = new Howl({
         src: [`./sounds/${type}/${type}_1.wav`]
     });
+    let pos = 0
     timer.on('tick', (ms) => {
-        sound.play();
-        console.log('tick', ms)})
-    timer.start(1000);
+        if(arr[pos]==1){
+            sound.play();
+            console.log('tick', ms)
+        }
+        pos = pos + 1
+        if(pos==arr.length){
+            pos = 0
+        }
+    })
+    timer.start(10000);
 }
